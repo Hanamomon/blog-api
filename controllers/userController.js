6a67883888ba50ed9d79a594802cb2a12cded6ba
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-05-24 00:50:23 -0700

    feat: implement authentication and authorization strategies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,33 @@
 import { findUserById, createUser, assignRole } from '../repository/userRepository.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export async function postUser(req, res) {
-  const { username, email, hash, salt } = req.body;
+  const { username, email, password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(password, salt);
 
   const user = await createUser({ username, email, hash, salt });
 
   res.status(200).json(user);
 }
 
+export async function logUser(req, res) {
+  const user = req.user;
+  const secret = process.env.JWT_SECRET;
+  
+  jwt.sign(user, secret, { expiresIn: '1d' }, (err, token) => {
+    if (err) {
+      return res.status(403).json({ error: err.message });
+    }
+    
+    res.status(200).json(token);
+  });
+}
+
 export async function patchRole(req, res) {
-  const { userId, role } = req.body;
+  const userId = req.user.id;
+  const { role } = req.body;
   
   const user = await assignRole({ userId, role });
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { postUser, patchRole } from "../controllers/userController.js";
+import { postUser, logUser, patchRole } from "../controllers/userController.js";
+import { passport } from "../lib/passport.js";
 
 const router = Router();
 
 router.post('/sign-up', postUser);
 
-router.patch('/:userId', patchRole);
+router.post('/login', passport.authenticate('local', { session: false, failureRedirect: '/login' }), logUser);
+
+router.patch('/:userId', passport.authenticate('jwt', { session: false, failureRedirect: '/login' }), patchRole);
 
 export default router;
