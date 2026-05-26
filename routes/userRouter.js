b961d0bbe97a657dd98e50f8d4e74b3ec615b348
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-05-25 19:41:04 -0700

    fix: send an error upon incorrect password on login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,17 @@ const router = Router();
 
 router.post('/sign-up', validateSignUp, validatorMiddleware, postUser);
 
-router.post('/login', validateLogin, validatorMiddleware, passport.authenticate('local', { session: false }), logUser);
+router.post('/login', validateLogin, validatorMiddleware,
+  (req,res, next) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      if (err) { return next(err); }
+      if (!user) { return res.status(401).json({ errors: { password: { msg: 'Incorrect password.'} } }); }
+
+      req.user = user;
+      next();
+    })(req, res, next);
+  }
+  , logUser);
 
 router.patch('/', passport.authenticate('jwt', { session: false }), validateRole, validatorMiddleware, patchRole);
 
